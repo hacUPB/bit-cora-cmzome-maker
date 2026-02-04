e40d6556a53e5ec7ff6f704d0bbb28579040c20d
cmzome-maker <cmzo.me@gmail.com>
2026-02-04 11:35:02 -0500

    Subo avance del ejercicio 3

--- Unidad 2/Actividad3.asm
+++ Unidad 2/Actividad3.asm
@@ -1,19 +1,18 @@
-RAM[20495]=-1
-Leer KBD
-if KBD=105       //i=105
-   RAM[20496]=0
-   RAM[20495]=-1
-if KBD=100       //d=100
-   RAM[20496]=0
-   RAM[20497]=-1
+@20495
+D=A
+@SCN
+M=D
 
+@SCN
+A=M
+M=-1
 
+(LOOP)
+@24576
+D=M
+@LOOP
+D;JEQ
 
-
-
-   @24576
-   A=M
-   
-   M=0
-
-
+@SCN
+A=M
+M=0
